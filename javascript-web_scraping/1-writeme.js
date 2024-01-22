af1fb7ec7b9f26ccbe7cefea2fca911a81a53001
JoseMilans <josemilans@gmail.com>
2024-01-22 23:26:46 +0000

    feat: Add script to write string to file

---
+++ javascript-web_scraping/1-writeme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+const [, , filePath, strToWrite] = process.argv;
+
+fs.writeFile(filePath, strToWrite, 'utf-8', (err) => {
+  if (err) {
+    console.error(err);
+  }
+});
