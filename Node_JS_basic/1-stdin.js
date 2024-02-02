7feabab174a61db6e7d666fafd9fb5a7e22bdc41
JoseMilans <josemilans@gmail.com>
2024-02-02 19:00:45 +0000

    fix: adjust end-of-line chars to pass test

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,6 +1,6 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('data', (input) => {
-  console.log(`Your name is: ${input.toString().trim()}`);
-  console.log('This important software is now closing');
+  process.stdout.write(`Your name is: ${input.toString().trim()}\r\n`);
+  process.stdout.write('This important software is now closing\r\n');
   process.exit();
 });
