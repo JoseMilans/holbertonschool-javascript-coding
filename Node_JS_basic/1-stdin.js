282aa11c249a04beedec90493f8085f193c2ca58
JoseMilans <josemilans@gmail.com>
2024-02-02 20:21:59 +0000

    fix: adjust output formatting to pass test

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,8 +1,7 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('data', (input) => {
   const name = input.toString().trim();
-  const formattedName = name.replace(/\n/g, '').replace(/\r/g, '');
-  console.log(`Your name is: ${formattedName}`);
-  console.log('This important software is now closing');
+  console.log(`Your name is: ${name.replace(/\n/g, '').replace(/\r/g, '')}\r`);
+  console.log('This important software is now closing\r');
   process.exit();
 });
