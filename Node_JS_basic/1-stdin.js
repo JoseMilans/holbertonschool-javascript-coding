310835f25f5da5861e783143d8a1d1814af4e17d
JoseMilans <josemilans@gmail.com>
2024-02-02 22:59:59 +0000

    fix: linting failure

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,7 +1,7 @@
 console.log('Welcome to Holberton School, what is your name?');
 process.stdin.setEncoding('utf8');
 process.stdin.on('readable', () => {
-  let input = process.stdin.read();
+  const input = process.stdin.read();
   if (input !== null) {
     console.log(`Your name is: ${input.trim()}`);
     console.log('This important software is now closing');
