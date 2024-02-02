5308970c78b7a1b383a534184268fa21b90a34f6
JoseMilans <josemilans@gmail.com>
2024-02-02 21:41:05 +0000

    feat: adjust newline chars

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,7 +1,12 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
-process.stdin.on('data', (input) => {
-  const trimmedInput = input.toString().trim();
-  process.stdout.write(`Your name is: ${trimmedInput}\r\n`);
-  process.stdout.write('This important software is now closing\r\n');
-  process.exit();
+rl.on('line', (input) => {
+  console.log(`Your name is: ${input}`);
+  console.log('This important software is now closing');
+  rl.close();
 });
