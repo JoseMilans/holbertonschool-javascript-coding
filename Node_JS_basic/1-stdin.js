73eeadafc1b721f91994fc96f8506c8080c63e78
JoseMilans <josemilans@gmail.com>
2024-02-02 22:55:43 +0000

    fix: stdin handling for user input

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,7 +1,7 @@
 console.log('Welcome to Holberton School, what is your name?');
 process.stdin.setEncoding('utf8');
 process.stdin.on('readable', () => {
-  const input = process.stdin.read();
+  let input = process.stdin.read();
   if (input !== null) {
     console.log(`Your name is: ${input.trim()}`);
     console.log('This important software is now closing');
